c131c9a550421d9152483ae8427b294136c12639
Muskan Gupta <muskangupta.2052@gmail.com>
2023-07-08 12:35:14 +0530

    Version 1 restored

--- change.js
+++ change.js
@@ -1,3 +1,2 @@
 console.log('change2');
-console.log('new_changes_aiyoo')
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 console.log('index.js');
 console.log('version2');
-console.log('newstrees');
-console.log('New Version')
+console.log('newstrees')
